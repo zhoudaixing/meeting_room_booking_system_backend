9042cf063ab0f1cd9ed10ea2bacfedc8a3ec9dc0
zdx <dreamzz2000@163.com>
2024-06-27 11:55:35 +0800

    feat: add sql init method

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -11,6 +11,8 @@ import { RegisterUserDto } from './dto/register-user.dto';
 import { User } from './entities/user.entity';
 import { RedisService } from 'src/redis/redis.service';
 import { md5 } from 'src/utils';
+import { Role } from './entities/role.entity';
+import { Permission } from './entities/permission.entity';
 
 @Injectable()
 export class UserService {
@@ -18,6 +20,12 @@ export class UserService {
 
   @InjectRepository(User)
   private userRepository: Repository<User>;
+
+  @InjectRepository(Role)
+  private roleRepository: Repository<Role>;
+
+  @InjectRepository(Permission)
+  private permissionRepository: Repository<Permission>;
 
   @Inject(RedisService)
   private redisService: RedisService;
@@ -54,5 +62,45 @@ export class UserService {
       this.logger.error(e, UserService);
       return '注册失败';
     }
+  }
+
+  async initData() {
+    const user1 = new User();
+    user1.username = 'zhangsan';
+    user1.password = md5('111111');
+    user1.email = 'xxx@xx.com';
+    user1.isAdmin = true;
+    user1.nickName = '张三';
+    user1.phoneNumber = '13233323333';
+
+    const user2 = new User();
+    user2.username = 'lisi';
+    user2.password = md5('222222');
+    user2.email = 'yy@yy.com';
+    user2.nickName = '李四';
+
+    const role1 = new Role();
+    role1.name = '管理员';
+
+    const role2 = new Role();
+    role2.name = '普通用户';
+
+    const permission1 = new Permission();
+    permission1.code = 'ccc';
+    permission1.description = '访问 ccc 接口';
+
+    const permission2 = new Permission();
+    permission2.code = 'ddd';
+    permission2.description = '访问 ddd 接口';
+
+    user1.roles = [role1];
+    user2.roles = [role2];
+
+    role1.permissions = [permission1, permission2];
+    role2.permissions = [permission1];
+
+    await this.permissionRepository.save([permission1, permission2]);
+    await this.roleRepository.save([role1, role2]);
+    await this.userRepository.save([user1, user2]);
   }
 }
