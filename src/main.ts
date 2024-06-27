0c63a10db421ef7aed2c687854e4804bb7448695
Zhou.DX <zhoudaixing@gmail.com>
2024-06-27 17:21:21 +0800

    feat: add format response and invoke record interceptors

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { FormatResponseInterceptor } from './format-response.interceptor';
+import { InvokeRecordInterceptor } from './invoke-record.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new FormatResponseInterceptor());
+  app.useGlobalInterceptors(new InvokeRecordInterceptor());
+
   const configService = app.get(ConfigService);
 
   await app.listen(configService.get('nest_server_port'));
